59beea708f4701c59b8f5566b5c83cfc14642197
Oleg Zaichkin <trueolegzaichkin@gmail.com>
2018-09-23 08:38:37 +0300

    tutorial end

--- angular-tour-of-heroes/src/app/hero.ts
+++ angular-tour-of-heroes/src/app/hero.ts
@@ -1,7 +1,4 @@
 export class Hero {
   id: number;
   name: string;
-  age: number;
-
-  female: string;
 }
--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
-import { Hero } from "./hero";
+import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: "Mr. Nice", female: "man", age: 12 },
-  { id: 12, name: "Narco", female: "man", age: 12 },
-  { id: 13, name: "Bombasto", female: "man", age: 12 },
-  { id: 14, name: "Celeritas", female: "man", age: 12 },
-  { id: 15, name: "Magneta", female: "man", age: 12 },
-  { id: 16, name: "RubberMan", female: "man", age: 12 },
-  { id: 17, name: "Dynama", female: "man", age: 12 },
-  { id: 18, name: "Dr IQ", female: "man", age: 12 },
-  { id: 19, name: "Magma", female: "man", age: 12 },
-  { id: 20, name: "Tornado", female: "man", age: 12 }
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
 ];
